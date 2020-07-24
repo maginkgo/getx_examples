2c96fd4d025b93739409d0c45287c59da2128160
maginkgo <maginkgo@gmail.com>
2020-07-24 11:51:01 -0300

    auth provider interface

--- firebase_auth_demo/lib/src/auth/providers/auth_provider.dart
+++ firebase_auth_demo/lib/src/auth/providers/auth_provider.dart
@@ -2,22 +2,33 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import '../models/user_model.dart';
 
-class FirebaseAuthProvider {
+abstract class AuthProvider {
+  Future<UserModel> getUser();
+  Future<UserModel> login();
+  Future<void> logout();
+  Stream<UserModel> onUserChanged;
+}
+
+class FirebaseAuthProvider extends AuthProvider {
   final _auth = FirebaseAuth.instance;
 
+  @override
   Future<UserModel> getUser() async {
     return _userFromFirebase(await _auth.currentUser());
   }
 
+  @override
   Stream<UserModel> get onUserChanged {
     return _auth.onAuthStateChanged.map((firebaseUser) => _userFromFirebase(firebaseUser));
   }
 
+  @override
   Future<UserModel> login() async {
     final authResult = await _auth.signInAnonymously();
     return _userFromFirebase(authResult.user);
   }
 
+  @override
   Future<void> logout() async {
     await _auth.signOut();
   }
